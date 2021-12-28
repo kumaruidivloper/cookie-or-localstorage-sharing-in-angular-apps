a97bd21b56841dc3cbddf055424fb514401df2a3
Kumar Shan <kumarshan@Kumars-MacBook-Pro.local>
2021-12-28 10:35:45 +0530

    lint- fix

--- student-example-com/src/app/app-routing.module.ts
+++ student-example-com/src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [ 
+const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path : 'login', component: LoginComponent},
-  { path : 'dashboard', component: DashboardComponent}, 
+  { path : 'dashboard', component: DashboardComponent},
 
 ];
 
